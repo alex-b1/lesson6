0ac10253e873b8a7884b26ee1dcb658bd318c126
Alex Bogomolov <a.bogomolov@balance-pl.ru>
2021-04-15 22:00:48 +0000

    lesson6

---
+++ cargo_carriage.rb
@@ -0,0 +1,6 @@
+require_relative './carriage.rb'
+
+class CargoCarriage < Carriage
+  TYPE = :cargo
+
+end
---
+++ passenger_carriage.rb
@@ -0,0 +1,5 @@
+require_relative './carriage.rb'
+
+class PassengerCarriage < Carriage
+  TYPE = :passenger
+end
